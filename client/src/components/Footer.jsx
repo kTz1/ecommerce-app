2e784ae64a305f697dfc56cecdc99adc804bbfb8
Simon Catalin <simoncatalin@Simons-MacBook-Air.local>
2021-09-21 22:55:24 +0300

    initial commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,136 @@
+import styled from "styled-components";
+import { Facebook, Twitter, Instagram, Pinterest, Room, Phone, EmailOutlined } from "@material-ui/icons";
+import { mobile } from "../responsive";
+
+
+const Container = styled.div`
+    display: flex;
+    ${mobile({ flexDirection: "column" })};
+`;
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo = styled.h1``;
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+`;
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${(props) => props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+    ${mobile({ display: "none" })};
+`;
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+    ${mobile({ backgroundColor: "#fff8f8" })};
+`;
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`;
+
+const Payment = styled.img`
+    width: 50%;
+`;
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>Genius.</Logo>
+                <Desc>
+                    There are many variations of passages of Lorem Ipsum available, but
+                    the majority have suffered alteration in some form, by injected
+                    humour, or randomised words which don’t look even slightly believable.
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3b5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="e4405f">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55acee">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="e60023">
+                        <Pinterest />
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <Room style={{marginRight: "10px"}} />622 Dixie Path , South Tobinchester 98336
+                </ContactItem>
+                <ContactItem>
+                    <Phone style={{marginRight: "10px"}} />+40 234 567
+                </ContactItem>
+                <ContactItem>
+                    <EmailOutlined style={{marginRight: "10px"}} />contact@genius.com
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer;
---
+++ client/src/components/Products.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import Product from "./Product";
+import { popularProducts } from "../data";
+
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+
+const Products = () => {
+    return (
+        <Container>
+            {popularProducts.map((item) => (
+                <Product item={item} key={item.id} />
+            ))}
+        </Container>
+    )
+}
+
+export default Products;
